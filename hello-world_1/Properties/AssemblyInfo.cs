f550f37870c9377091690479451814c72448a6b5
manuel.mann <manuel.mann@TCL-MANUEL>
2023-03-28 14:24:01 +0200

    Add project files.

---
+++ hello-world_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("hello-world_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("hello-world_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("EC44A0E3-56F7-425E-9268-75307FAB44C7")]
+[assembly: AssemblyVersion("1.0.0.0")]
